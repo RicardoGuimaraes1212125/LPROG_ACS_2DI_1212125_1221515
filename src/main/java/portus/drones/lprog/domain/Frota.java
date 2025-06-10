8c6f5a431e7958df08f9a6d23b5d482fba5252d3
Carlos Almeida <carlos@carlosmcalmeida.pt>
2025-06-10 15:29:42 +0100

    created Frota classe to have accessible lists of models and drones in the whole program

--- src/main/java/portus/drones/lprog/domain/Frota.java
+++ src/main/java/portus/drones/lprog/domain/Frota.java
@@ -1,4 +1,10 @@
 package portus.drones.lprog.domain;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Frota {
+
+    public static final List<Drone> drones = new ArrayList<>();
+    public static final List<Modelo> modelos = new ArrayList<>();
 }
